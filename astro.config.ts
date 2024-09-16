3de25803375b4c169c1f409b666d6a0aeaceab03
Frikadellios <devopsick@pm.me>
2024-09-16 15:00:11 +0300

    add autoimport plugin

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'astro/config'
-
 import cloudflare from '@astrojs/cloudflare'
+import { defineConfig } from 'astro/config'
+import AutoImport from 'unplugin-auto-import/astro'
 
 import react from '@astrojs/react'
 
@@ -26,5 +26,23 @@ export default defineConfig({
     }
   }),
 
-  integrations: [react()]
+  integrations: [
+    AutoImport({
+      defaultExportByFilename: false,
+      include: [
+        /\.md$/, // .md
+        /\.[tj]sx?$/ // .ts, .tsx, .js, .jsx
+      ],
+      packagePresets: ['detect-browser-es'],
+      viteOptimizeDeps: true,
+      imports: ['react', 'react-router'],
+      dirs: ['./src/utils/*.ts', './src/hooks/*'],
+      dts: './src/auto-imports.d.ts',
+      biomelintrc: {
+        enabled: false, // Default `false`
+        filepath: './.biomelintrc-auto-import.json' // Default `./.biomelintrc-auto-import.json`
+      }
+    }),
+    react()
+  ]
 })
---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+export function Card() {
+  const [state, setState] = useState(0)
+  return (
+    <>
+      <h1>
+        React:
+        {state}
+      </h1>
+      <button type="button" onClick={() => setState(state + 1)}>
+        Add
+      </button>
+    </>
+  )
+}
